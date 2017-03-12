855ba7d594066477ad251033d1515b8b801b0b58
Mark ter Maat <mark.termaat@nedap.com>
2017-03-12 14:41:50 +0100

    Updated to elixir 1.4.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mealplanner.Mixfile do
   def project do
     [app: :mealplanner,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
