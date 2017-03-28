34164c44ad4edc008a3211b42bce7e93edd3fb89
Mark ter Maat <mark.termaat@nedap.com>
2017-03-28 20:25:43 +0200

    Added snooze button that only pops up when a meal is selected

--- web/static/js/meals.js
+++ web/static/js/meals.js
@@ -3,8 +3,17 @@ import $ from "jquery"
 export var Meals = {
     init: (channel) => {
         $('.js-meals').on("click", ".js-meal", (event) => {
+            $(".selected-meal").removeClass("selected-meal")
+            $(event.currentTarget).addClass("selected-meal")
             const mealname = $(event.currentTarget).children(".js-mealname").text()
             $(".js-newmealform input[name='name']").val(mealname)
         })
+
+        // Remove the selection when the user clicks somewhere else
+        $(document).on('click', function (event) {
+            if (!$(event.target).closest('.js-meals').length) {
+                $(".selected-meal").removeClass("selected-meal")
+            }
+        });
     }
 }
